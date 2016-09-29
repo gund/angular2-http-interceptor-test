e832c56ba39b2ae7ea8bbb882ba07247cec061e7
Alex Malkevich <malkevich.alex@gmail.com>
2016-09-30 01:45:32 +0400

    Write unit tests for interceptable-http.ts

--- src/app/core/http/interceptable-http.spec.ts
+++ src/app/core/http/interceptable-http.spec.ts
@@ -1,9 +1,8 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { InterceptableHttp } from './interceptable-http';
-import { HTTP_INTERCEPTOR_PROVIDER } from './providers';
-import { HttpModule, XHRBackend } from '@angular/http';
+import { HttpModule, XHRBackend, Http, ConnectionBackend } from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
 
 describe('Service: InterceptableHttp', () => {
@@ -12,12 +11,17 @@ describe('Service: InterceptableHttp', () => {
       imports: [HttpModule],
       providers: [
         {provide: XHRBackend, useClass: MockBackend},
-        ...HTTP_INTERCEPTOR_PROVIDER
+        {provide: ConnectionBackend, useClass: MockBackend},
+        InterceptableHttp
       ]
     });
   });
 
   it('should exist', inject([InterceptableHttp], (service: InterceptableHttp) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should extend Http service', inject([InterceptableHttp, Http], (service: InterceptableHttp, http: Http) => {
+    expect(service).toEqual(jasmine.any(http.constructor));
   }));
 });
